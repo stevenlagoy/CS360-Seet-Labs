0ecd883d264358425f527d7852b81bfb18e53c9e
Fuzzy39 <jmcraft126@gmail.com>
2025-03-18 22:06:58 -0400

    basic layout

--- seetLabs/src/app/pages/playground/playground.component.ts
+++ seetLabs/src/app/pages/playground/playground.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { GradientHeaderComponent } from '../../components/gradient-header/gradient-header.component';
+import { QuizCardComponent } from '../../components/quiz-card/quiz-card.component';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TextAreaComponent } from '../../components/text-area/text-area.component';
+import { InputFieldComponent } from '../../components/input-field/input-field.component';
+
 
 @Component({
   selector: 'app-playground',
-  imports: [],
+  imports: [MatButtonModule,
+    MatIconModule,
+    GradientHeaderComponent,
+    QuizCardComponent,
+    MatProgressBar,
+    MatProgressSpinnerModule,
+    ProgressBarComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    InputFieldComponent,
+    TextAreaComponent
+  ],
   templateUrl: './playground.component.html',
   styleUrl: './playground.component.css'
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent 
+{
+
+  ngOnInit()
+  {
+    this.init();
+
+  }
+
+  private async init()
+  {
+    await cheerpjInit( /*{natives: { Java_JSOutputStream_jsWrite }}*/ );
+    let val = await cheerpjRunMain(
+        "Main",
+        "/app/ClearFiles.jar",
+    );
+    if(await val !== 0)
+    {
+        alert("Initialization failed.");
+        return;
+    }
+
+    alert("Initialization success!");
+    //document.getElementById("compile").removeAttribute("disabled");
+    //document.getElementById("status").innerHTML = "Status: Ready";
+  } 
 
 }
