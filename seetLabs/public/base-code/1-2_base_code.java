5dc7c9891585a6240a102c9144729e5381c7bc86
stevenlagoy <stevenlagoy@gmail.com>
2025-04-21 21:56:07 -0400

    Change functions in basecode files for ConsoleOutput to be called main()

--- seetLabs/public/base-code/1-2_base_code.java
+++ seetLabs/public/base-code/1-2_base_code.java
@@ -1,5 +1,5 @@
 public class Lab {
-	public static void Variables() {
+	public static void main(String[] args) {
 		// Declare and initialize variables below
 		
 		// Print all values using System.out.println()
