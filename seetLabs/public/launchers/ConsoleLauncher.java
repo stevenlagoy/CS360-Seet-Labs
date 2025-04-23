ae4c41f900029b6f73084666fdd1ef7ef1ad8b45
Fuzzy39 <mshill126@gmail.com>
2025-04-23 09:43:47 -0400

    Got Console output activities to work

---
+++ seetLabs/public/launchers/ConsoleLauncher.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ConsoleLauncher
+{
+    public static void main(String[] args)
+    {
+        LabLauncher launcher = new LabLauncher();
+        launcher.getUserMethod("main", Void.TYPE, args.getClass());
+        String[] fakeArg = new String[0];
+
+        launcher.launchMethod(new Object[]{fakeArg});
+
+        // other launchers should exit 0 if the user's code was successful, and 1 otherwise.
+        
+      
+
+        String testAgainst ="";
+        try(Scanner sc = new Scanner(new File(args[0])))
+        {
+            while(sc.hasNext()) 
+            {
+                testAgainst+=sc.nextLine()+"\n";
+            }     
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+
+        int toReturn = launcher.getJSOutputStream().getOutput().trim().equals(testAgainst.trim())?0:1;
+        if(toReturn==1)
+        {
+            System.err.println("Test Failed. Expected Output: \n"+testAgainst+"");
+        }
+        System.exit(toReturn);    
+    }
+}
---
+++ seetLabs/public/launchers/TestCaseLauncher.java
@@ -0,0 +1,10 @@
+public class TestCaseLauncher 
+{
+    public static void main(String[] args)
+    {
+        LabLauncher launcher = new LabLauncher();
+        System.out.println("Java ran successfully.");
+        System.exit(1);
+    }
+
+}
